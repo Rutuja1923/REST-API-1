3208384a4d635af321afd5c66550984c12bc2623
Rutuja1923 <rutujajirage19@gmail.com>
2025-01-25 13:36:11 +0530

    Server containg route description

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const users = require('./MOCK_DATA.json');
+
+const app = express();
+const port = 3000;
+
+//routes
+app.get('/',(req,res) => {
+    const link = `http://localhost:${port}/api/users`;
+    res.send(`Hey there! please try <a href='http://localhost:${port}/api/users'>http://localhost:${port}/api/users</a>`);
+});
+
+app.get('/users',(req,res) => {
+    const html = 
+    `   <ul>
+            ${users.map((user) =>
+                    `<li>${user.first_name} ${user.last_name} (${user.job_title})</li>`
+                ).join('')
+            }
+        </ul>
+    `;
+    res.send(html);
+});
+
+//REST API routes
+app.get('/api/users',(req,res) => {
+    return res.json(users);
+});
+ 
+app.get('/api/users/:id',(req,res) => {
+    const id = Number(req.params.id);
+    const user = users.find( (user) =>
+        user.id === id
+    );
+    return res.json(user);
+});   
+
+app.post('/api/users',(req,res) => {
+    //TODO : Create a new User
+    return res.json(
+        {
+            status : 'pending'
+        }
+    );
+});
+
+app.patch('/api/users/:id',(req,res) => {
+    //TODO : Edit the user with the id
+    return res.json(
+        {
+            status : 'pending'
+        }
+    );
+});
+
+app.delete('/api/users/:id',(req,res) => {
+    //TODO : Delete the user with the id
+    return res.json(
+        {
+            status : 'pending'
+        }
+    );
+});
+
+app.listen(port, () => {
+    console.log(`Server stared at http://localhost:${port}`)
+});
