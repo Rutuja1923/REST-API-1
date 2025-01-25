ec891355effe7376fb1744d170eedb4625b012eb
Rutuja1923 <rutujajirage19@gmail.com>
2025-01-25 13:47:30 +0530

    Refactor: group /api/users/:id methods

--- index.js
+++ index.js
@@ -27,34 +27,34 @@ app.get('/api/users',(req,res) => {
     return res.json(users);
 });
  
-app.get('/api/users/:id',(req,res) => {
-    const id = Number(req.params.id);
-    const user = users.find( (user) =>
-        user.id === id
-    );
-    return res.json(user);
-});   
+app
+    .route('/api/users/:id')
+    .get((req,res) => {
+        const id = Number(req.params.id);
+        const user = users.find( (user) =>
+            user.id === id
+        );
+        return res.json(user);
+    })
+    .patch((req,res) => {
+        //TODO : Edit the user with the id
+        return res.json(
+            {
+                status : 'pending'
+            }
+        );
+    })
+    .delete((req,res) => {
+        //TODO : Delete the user with the id
+        return res.json(
+            {
+                status : 'pending'
+            }
+        );
+    });   
 
 app.post('/api/users',(req,res) => {
     //TODO : Create a new User
-    return res.json(
-        {
-            status : 'pending'
-        }
-    );
-});
-
-app.patch('/api/users/:id',(req,res) => {
-    //TODO : Edit the user with the id
-    return res.json(
-        {
-            status : 'pending'
-        }
-    );
-});
-
-app.delete('/api/users/:id',(req,res) => {
-    //TODO : Delete the user with the id
     return res.json(
         {
             status : 'pending'
